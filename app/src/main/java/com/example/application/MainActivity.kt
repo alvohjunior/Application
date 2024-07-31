400454830f5446e43a708128fbf1d1e7901df263
alvohjunior <alvinwanjiku123@gmail.com>
2024-07-31 11:23:04 +0300

    Commit message

---
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.application.ui.theme.ApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Alvin",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .clip(shape = RoundedCornerShape(10.dp))
+
+
+    ){
+        Text(text = "GoodMorning $name",
+            fontSize = 20.sp,
+            color = Color.Blue,
+            fontFamily = FontFamily.SansSerif,
+            fontStyle = FontStyle.Normal,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.White)
+                .padding(20.dp)
+                .fillMaxWidth()
+
+
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    Image(modifier = Modifier
+        .wrapContentHeight()
+        .fillMaxWidth()
+        .height(100.dp),
+        painter = painterResource(id = R.drawable.img), contentDescription =" img")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ApplicationTheme {
+        Greeting("Alvin")
+    }
+}
